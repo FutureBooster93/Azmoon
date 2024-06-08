c395a42c0bb8a1213d0104149ee2a838762671be
FutureBooster93 <rezaerfani1993@gmail.com>
2024-06-08 04:22:10 +0330

    completion of SecondReports controller and the three actions of index, reportconfirm and preReport

--- Azmoon/ViewModels/ReportsViewModel.cs
+++ Azmoon/ViewModels/ReportsViewModel.cs
@@ -2,10 +2,20 @@
 {
     public class ReportsViewModel
     {
+        public ReportsViewModel()
+        {
+            Id = new();
+            FirstName = new();
+            LastName = new();
+            PostalCode = new();
+            Tel = new();
+            Email = new();
+            combinedList = new();
+        }
         public List<int>? Id { get; set; }
         public List<string>? FirstName { get; set; }
-        public List<string>? LastName{ get; set; }
-        public List<string>? PostalCode{ get; set; }
+        public List<string>? LastName { get; set; }
+        public List<string>? PostalCode { get; set; }
         public List<string>? Tel { get; set; }
         public List<string>? Email { get; set; }
         public string? Title { get; set; }
